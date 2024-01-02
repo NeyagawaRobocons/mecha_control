3888469deebffd0225c3c91e1535efb7c277c5c1
tonto2423 <tnt.kosen2423@gmail.com>
2024-01-02 12:47:15 +0900

    feat: show the commands on loginfo

--- mecha_control/dummy_node.py
+++ mecha_control/dummy_node.py
@@ -1,4 +1,50 @@
 #!/usr/bin/env python3
+"""
+## メッセージの中身
+### 台座機構
+
+#### `/daiza_clamp`トピック: `ActuatorCommands.msg`（アクチュエータ指令）
+- `cylinder_states`（シリンダの状態）
+  - `0`: シリンダ1
+  - `1`: シリンダ2
+  - `2`: シリンダ3
+- `motor_positions`（モータの位置）
+  - `0`: 角度調整モータ
+
+#### `/daiza_state`トピック: `SensorStates.msg`（センサ状態）
+- `limit_switch_states`（リミットスイッチの状態）
+  - `0`: リミットスイッチ(上)
+  - `1`: リミットスイッチ(下)
+  - `2`: リミットスイッチ(台座)
+- `cylinder_states` (シリンダの状態)
+  - `0`: シリンダ1
+  - `1`: シリンダ2
+  - `2`: シリンダ3
+
+### 人形機構
+
+#### `/hina_dastpan`トピック: `ActuatorCommands.msg`（アクチュエータ指令）
+- `cylinder_states`（シリンダの状態）
+  - `0`: シリンダ1
+  - `1`: シリンダ2
+- `motor_positions`（モータの位置）
+  - `0`: モータ1
+  - `1`: モータ2
+
+#### `/hina_state`トピック: `SensorStates.msg`（センサ状態）
+- `limit_switch_states`（リミットスイッチの状態）
+  - `0`: リミットスイッチ(180)1
+  - `1`: リミットスイッチ(180)2
+  - `2`: リミットスイッチ(壁)1
+  - `3`: リミットスイッチ(壁)2
+- `cylinder_states` (シリンダの状態)
+  - `0`: シリンダ1
+  - `1`: シリンダ2
+- `potentiometer_angles`（ポテンショメータの角度）
+  - `0`: ポテンショメータ1
+  - `1`: ポテンショメータ2
+"""
+
 import rclpy
 from rclpy.node import Node
 from mecha_control.msg import ActuatorCommands, SensorStates
@@ -7,16 +53,31 @@ import tkinter as tk
 class DummyNode(Node):
     def __init__(self):
         super().__init__('dummy_node')
-        self.publisher = self.create_publisher(SensorStates, '/sensor_states', 10)
+        self.daiza_publisher = self.create_publisher(SensorStates, '/daiza_state', 10)
+        self.daiza_states = SensorStates()
+        self.hina_publisher = self.create_publisher(SensorStates, '/hina_state', 10)
+        self.hina_states = SensorStates()
         self.subscription_daiza = self.create_subscription(
             ActuatorCommands, '/daiza_clamp', self.actuator_callback_daiza, 10)
         self.subscription_hina = self.create_subscription(
             ActuatorCommands, '/hina_dastpan', self.actuator_callback_hina, 10)
         
-        self.sensor_states = SensorStates()
+        # daiza's variables
+        self.daiza_states.limit_switch_states = [False] * 3
+        self.daiza_states.cylinder_states = [False] * 3
+        # daiza_commands 配列の初期化
+        self.daiza_commands = ActuatorCommands()
+        self.daiza_commands.cylinder_states = [False] * 3
+        self.daiza_commands.motor_positions = [0.0]
 
-        # limit_switch_states 配列の初期化
-        self.sensor_states.limit_switch_states = [False] * 6  # 配列のサイズに応じて調整
+        # hina's variables
+        self.hina_states.limit_switch_states = [False] * 4
+        self.hina_states.cylinder_states = [False] * 2
+        self.hina_states.potentiometer_angles = [0.0] * 2
+        # hina_commands 配列の初期化
+        self.hina_commands = ActuatorCommands()
+        self.hina_commands.cylinder_states = [False] * 2
+        self.hina_commands.motor_positions = [0.0] * 2
 
         self.init_gui()
 
@@ -49,21 +110,28 @@ class DummyNode(Node):
             "人形": {"180°": 0, "壁": 1}
         }
         index = index_map[kikou][switch]
-        self.sensor_states.limit_switch_states[index] = not self.sensor_states.limit_switch_states[index]
-        self.publisher.publish(self.sensor_states)
+        if kikou == "台座":
+            self.daiza_states.limit_switch_states[index] = not self.daiza_states.limit_switch_states[index]
+        elif kikou == "人形":
+            self.hina_states.limit_switch_states[index] = not self.hina_states.limit_switch_states[index]
+        self.daiza_publisher.publish(self.daiza_states)
+        self.hina_publisher.publish(self.hina_states)
 
     def actuator_callback_daiza(self, msg):
         # 受け取ったメッセージから台座機構のシリンダ状態を更新
-        self.sensor_states.limit_switch_states[3] = msg.cylinder_states[0]  # シリンダ1
-        self.sensor_states.limit_switch_states[4] = msg.cylinder_states[1]  # シリンダ2
-        self.sensor_states.limit_switch_states[5] = msg.cylinder_states[2]  # シリンダ3
-        self.publisher.publish(self.sensor_states)
+        self.daiza_states.cylinder_states[0] = msg.cylinder_states[0]  # シリンダ1
+        self.daiza_states.cylinder_states[1] = msg.cylinder_states[1]
+        self.daiza_states.cylinder_states[2] = msg.cylinder_states[2]
+        self.daiza_publisher.publish(self.daiza_states)
 
     def actuator_callback_hina(self, msg):
         # 受け取ったメッセージから人形機構のシリンダ状態を更新
-        self.sensor_states.limit_switch_states[4] = msg.cylinder_states[0]  # シリンダ1
-        self.sensor_states.limit_switch_states[5] = msg.cylinder_states[1]  # シリンダ2
-        self.publisher.publish(self.sensor_states)
+        self.hina_states.cylinder_states[0] = msg.cylinder_states[0]
+        self.hina_states.cylinder_states[1] = msg.cylinder_states[1]
+        self.hina_publisher.publish(self.hina_states)
+
+    def show_commands(self):
+        self.get_logger("台座機構の指令: ", self.daiza_commands, "人形機構の指令: ", self.hina_commands)
 
 def main(args=None):
     rclpy.init(args=args)
