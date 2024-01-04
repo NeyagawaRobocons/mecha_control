4eee724b5792953ecdd1b78f26204c8bfdc1f99e
tonto2423 <tnt.kosen2423@gmail.com>
2024-01-04 22:50:47 +0900

    refactor: コードを見やすくした

--- src/sequence_controller.cpp
+++ src/sequence_controller.cpp
@@ -62,10 +62,7 @@ private:
         // 台座機構の処理
         if (!(msg->daiza_state == 0)) {
             // 台座機構の目標状態に応じた処理をここに実装
-            switch (msg->daiza_state)
-            {
-            case 0:
-                break;
+            switch (msg->daiza_state) {
             case 1: // 展開
                 std::cout << "台座機構の展開" << std::endl;
                 // 展開用シリンダを動かす指令を送信
@@ -78,17 +75,20 @@ private:
                 break;
             case 2: // 回収
                 std::cout << "台座機構の回収" << std::endl;
-                if (!current_daiza_state_.limit_switch_states.empty() && current_daiza_state_.limit_switch_states[2]) {
-                    // シリンダ(小)を縮める指令を送信（小台座を倒す）
-                    daiza_commands.cylinder_states[2] = false;
-                    daiza_commands_publisher_->publish(daiza_commands);
-                    // this->delay_rate.sleep();   // 1秒待つ
-                    delay_with_log();
-                    // シリンダ(右), (左)を縮める: 台座を挟む
-                    daiza_commands.cylinder_states[0] = false;
-                    daiza_commands.cylinder_states[1] = false;
-                    daiza_commands_publisher_->publish(daiza_commands);
+                if (!current_daiza_state_.limit_switch_states.empty()) {
+                    if (current_daiza_state_.limit_switch_states[2]) {
+                        // シリンダ(小)を縮める指令を送信（小台座を倒す）
+                        daiza_commands.cylinder_states[2] = false;
+                        daiza_commands_publisher_->publish(daiza_commands);
+                        // this->delay_rate.sleep();   // 1秒待つ
+                        delay_with_log();
+                        // シリンダ(右), (左)を縮める: 台座を挟む
+                        daiza_commands.cylinder_states[0] = false;
+                        daiza_commands.cylinder_states[1] = false;
+                        daiza_commands_publisher_->publish(daiza_commands);
+                    }
                 }
+                else { std::cout << "limit_switch_states is empty" << std::endl; }
                 delay_with_log();   // 1秒待つ
                 // 展開用シリンダを動かす指令を送信(縮める)
                 daiza_commands.cylinder_states[3] = false;
@@ -110,15 +110,17 @@ private:
         } 
         // 人形機構の処理
         else if (!(msg->hina_state == 0)) {
-            switch (msg->hina_state)
-            {
+            switch (msg->hina_state) {
             case 4: // 準備
                 std::cout << "人形機構の準備" << std::endl;
                 // モータ2: ポテンショメータが90°になるまで展開
-                if (!current_hina_state_.potentiometer_angles.empty() && current_hina_state_.potentiometer_angles[1] < 90.0) {
-                    hina_commands.motor_positions[1] = 90.0;
-                    hina_commands_publisher_->publish(hina_commands);
+                if (!current_hina_state_.potentiometer_angles.empty()) { 
+                    if (current_hina_state_.potentiometer_angles[1] < 90.0) {
+                        hina_commands.motor_positions[1] = 90.0;
+                        hina_commands_publisher_->publish(hina_commands);
+                    }
                 }
+                else { std::cout << "potentiometer_angles is empty" << std::endl; }
                 // モータ1: リミットスイッチ(下)が押されるまで縮める
                 hina_commands.motor_expand[0] = false;
                 hina_commands_publisher_->publish(hina_commands);
@@ -126,46 +128,59 @@ private:
             case 1: // 展開
                 std::cout << "人形機構の展開" << std::endl;
                 // モータ2: ポテンショメータが-10° (調整できるようにする)になるまで動かす
-                if (!current_hina_state_.potentiometer_angles.empty() && current_hina_state_.potentiometer_angles[1] > -10.0) {
-                    hina_commands.motor_positions[1] = -10.0;
-                    hina_commands_publisher_->publish(hina_commands);
-                }
-                break;
-            case 2: // 回収
-                std::cout << "人形機構の回収" << std::endl;
-                // if (リミットスイッチ(壁)1, 2が両方押されたら)
-                if (!current_hina_state_.limit_switch_states.empty() && current_hina_state_.limit_switch_states[2] && current_hina_state_.limit_switch_states[3]) {
-                    // モータ2: テンショメータ1,2が90°になるまで縮める
-                    if (!current_hina_state_.potentiometer_angles.empty() && current_hina_state_.potentiometer_angles[1] < 90.0) {
-                        hina_commands.motor_positions[1] = 90.0;
-                        hina_commands_publisher_->publish(hina_commands);
-                    }
-                    // モータ1: リミットスイッチ(下)が押されるまで縮める
-                    hina_commands.motor_expand[0] = false;
-                    hina_commands_publisher_->publish(hina_commands);
-                }
-                break;
-            case 3: // 設置
-                std::cout << "人形機構の設置" << std::endl;
-                // if (リミットスイッチ(上)が押されいる)
-                if (!current_hina_state_.limit_switch_states.empty() && current_hina_state_.limit_switch_states[0]) {
-                    // モータ2: ポテンショメータが-10° (調整できるようにする)になるまで動かす
+                if (!current_hina_state_.potentiometer_angles.empty()) {
                     if (current_hina_state_.potentiometer_angles[1] > -10.0) {
                         hina_commands.motor_positions[1] = -10.0;
                         hina_commands_publisher_->publish(hina_commands);
                     }
-                    delay_with_log();   // 1秒待つ
-                    // モータ2: テンショメータが90°になるまで縮める
-                    if (current_hina_state_.potentiometer_angles[1] < 90.0) {
-                        hina_commands.motor_positions[1] = 90.0;
+                }
+                else { std::cout << "potentiometer_angles is empty" << std::endl; }
+                break;
+            case 2: // 回収
+                std::cout << "人形機構の回収" << std::endl;
+                // if (リミットスイッチ(壁)1, 2が両方押されたら)
+                if (!current_hina_state_.limit_switch_states.empty()) {
+                    if (current_hina_state_.limit_switch_states[2] && current_hina_state_.limit_switch_states[3]) {
+                        // モータ2: テンショメータ1,2が90°になるまで縮める
+                        if (current_hina_state_.potentiometer_angles[1] < 90.0) {
+                            hina_commands.motor_positions[1] = 90.0;
+                            hina_commands_publisher_->publish(hina_commands);
+                        }
+                        // モータ1: リミットスイッチ(下)が押されるまで縮める
+                        hina_commands.motor_expand[0] = false;
                         hina_commands_publisher_->publish(hina_commands);
                     }
-                    // ぼんぼり点灯をする
                 }
+                else { std::cout << "limit_switch_states is empty" << std::endl; }
+                break;
+            case 3: // 設置
+                std::cout << "人形機構の設置" << std::endl;
+                // if (リミットスイッチ(上)が押されいる)
+                if (!current_hina_state_.limit_switch_states.empty()) {
+                    if (current_hina_state_.limit_switch_states[0]) {
+                        // モータ2: ポテンショメータが-10° (調整できるようにする)になるまで動かす
+                        if (current_hina_state_.potentiometer_angles[1] > -10.0) {
+                            hina_commands.motor_positions[1] = -10.0;
+                            hina_commands_publisher_->publish(hina_commands);
+                        }
+                        delay_with_log();   // 1秒待つ
+                        // モータ2: テンショメータが90°になるまで縮める
+                        if (current_hina_state_.potentiometer_angles[1] < 90.0) {
+                            hina_commands.motor_positions[1] = 90.0;
+                            hina_commands_publisher_->publish(hina_commands);
+                        }
+                        // ぼんぼり点灯をする
+                    }
+                }
+                else { std::cout << "limit_switch_states is empty" << std::endl; }
                 break;
             default:
                 break;
             }
+        }
+        else if (msg->bonbori_state) {
+            // ぼんぼり点灯をする
+            std::cout << "ぼんぼり点灯" << std::endl;
         }
     }
     void delay_with_log() {
