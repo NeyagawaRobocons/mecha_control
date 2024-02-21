fc4e183cdad1a368ab90139cdb02642563147e6d
paripal <paripal5z34@gmail.com>
2024-02-22 04:41:10 +0900

    cannot build

---
+++ src/cmd_seq_class.hpp
@@ -0,0 +1,342 @@
+#pragma once
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "mecha_control/srv/mech_cmd.hpp"
+#include "mecha_control/action/mech_cmd.hpp"
+#include "mecha_control/action/daiza_cmd.hpp"
+#include "mecha_control/action/hina_cmd.hpp"
+#include "std_srvs/srv/set_bool.hpp"
+#include "mecha_control/msg/actuator_commands.hpp"
+#include "mecha_control/msg/sensor_states.hpp"
+#include <thread>
+#include <mutex>
+#include <unistd.h>
+
+#include "mech.hpp"
+
+class MechSeqNode : public rclcpp::Node
+{
+public:
+    using DaizaCmd = mecha_control::action::DaizaCmd;
+    using GoalHandleDaizaCmd = rclcpp_action::ServerGoalHandle<DaizaCmd>;
+    using HinaCmd = mecha_control::action::HinaCmd;
+    using GoalHandleHinaCmd = rclcpp_action::ServerGoalHandle<HinaCmd>;
+    MechSeqNode()
+    : Node("mech_cmd_seq_node")
+    {
+        this->mech = Mech(this);
+        using namespace std::placeholders;
+        this->daiza_action_server = rclcpp_action::create_server<DaizaCmd>(
+            this,
+            "daiza_cmd",
+            std::bind(&MechSeqNode::daiza_handle_goal, this, _1, _2),
+            std::bind(&MechSeqNode::daiza_handle_cancel, this, _1),
+            std::bind(&MechSeqNode::daiza_handle_accepted, this, _1));
+        bonbori_service = this->create_service<std_srvs::srv::SetBool>(
+            "set_bonbori", std::bind(&MechSeqNode::set_bonbori_callback, this, std::placeholders::_1, std::placeholders::_2));
+        RCLCPP_INFO(this->get_logger(), "node started");
+    }
+
+private:
+    Mech mech;
+    rclcpp_action::Server<DaizaCmd>::SharedPtr daiza_action_server;
+    rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr bonbori_service;
+
+    rclcpp_action::GoalResponse daiza_handle_goal(
+        const rclcpp_action::GoalUUID & uuid,
+        std::shared_ptr<const DaizaCmd::Goal> goal)
+    {
+        RCLCPP_INFO(this->get_logger(), "Received goal request with order %d", goal->command);
+        (void)uuid;
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }
+    rclcpp_action::CancelResponse daiza_handle_cancel(
+        const std::shared_ptr<GoalHandleDaizaCmd> goal_handle)
+    {
+        RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+        (void)goal_handle;
+        return rclcpp_action::CancelResponse::ACCEPT;
+    }
+    void daiza_handle_accepted(const std::shared_ptr<GoalHandleDaizaCmd> goal_handle)
+    {
+        using namespace std::placeholders;
+        // this needs to return quickly to avoid blocking the executor, so spin up a new thread
+        std::thread{std::bind(&MechSeqNode::daiza_cmd_execute, this, _1), goal_handle}.detach();
+    }
+    void daiza_cmd_execute(const std::shared_ptr<GoalHandleDaizaCmd> goal_handle)
+    {
+        RCLCPP_INFO(this->get_logger(), "Executing goal");
+        rclcpp::Rate loop_rate(1);
+        const auto goal = goal_handle->get_goal();
+        auto feedback = std::make_shared<DaizaCmd::Feedback>();
+        auto result = std::make_shared<DaizaCmd::Result>();
+
+        rclcpp::WallRate loop(0.1);
+        u_int8_t step = 0;
+        for (int i = 0; i < 100 && rclcpp::ok(); i++) {
+            // Check if there is a cancel request
+            if (goal_handle->is_canceling()) {
+                result->result = DaizaCmd::Result::ABORTED;
+                goal_handle->canceled(result);
+                RCLCPP_INFO(this->get_logger(), "Goal canceled");
+                return;
+            }
+            switch (goal)
+            {
+            case DaizaCmd::Goal::STOP:
+                result->result = DaizaCmd::Result::OK;
+                goal_handle->succeed(result);
+                RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::STOP OK");
+                return;
+                break;
+            case DaizaCmd::Goal::READY:
+                struct DaizaActuator act;
+                act.expand = 0;
+                act.clamp  = 1;
+                act.guide_close = 1;
+                mech.set_daiza(act);
+
+                struct DaizaState state = get_daiza();
+                if(state.is_expand == 0 && state.is_clamp == 1 && state.is_guide_close == 1){
+                    result->result = DaizaCmd::Result::OK;
+                    goal_handle->succeed(result);
+                    RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::READY OK");
+                    return;
+                }
+                break;
+            case DaizaCmd::Goal::EXPAND:
+                struct DaizaState state = get_daiza();
+                struct DaizaActuator act;
+                act.expand = 1;
+                act.clamp  = state.is_clamp;
+                act.guide_close = state.is_guide_close;
+                mech.set_daiza(act);
+
+                if(state.is_expand == 1){
+                    result->result = DaizaCmd::Result::OK;
+                    goal_handle->succeed(result);
+                    RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::EXPAND OK");
+                    return;
+                }
+                break;
+            case DaizaCmd::Goal::CONTRACT:
+                struct DaizaState state = get_daiza();
+                struct DaizaActuator act;
+                act.expand = 0;
+                act.clamp  = state.is_clamp;
+                act.guide_close = state.is_guide_close;
+                mech.set_daiza(act);
+
+                if(state.is_expand == 0){
+                    result->result = DaizaCmd::Result::OK;
+                    goal_handle->succeed(result);
+                    RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::CONTRACT OK");
+                    return;
+                }
+                break;
+            case DaizaCmd::Goal::CLAMP:
+                struct DaizaState state = get_daiza();
+                struct DaizaActuator act;
+                act.expand = state.is_expand;
+                act.clamp  = 1;
+                act.guide_close = state.is_guide_close;
+                mech.set_daiza(act);
+
+                if(state.is_clamp == 1){
+                    result->result = DaizaCmd::Result::OK;
+                    goal_handle->succeed(result);
+                    RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::CLAMP OK");
+                    return;
+                }
+                break;
+            case DaizaCmd::Goal::UNCLAMP:
+                struct DaizaState state = get_daiza();
+                struct DaizaActuator act;
+                act.expand = state.is_expand;
+                act.clamp  = 0;
+                act.guide_close = state.is_guide_close;
+                mech.set_daiza(act);
+
+                if(state.is_clamp == 0){
+                    result->result = DaizaCmd::Result::OK;
+                    goal_handle->succeed(result);
+                    RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::UNCLAMP OK");
+                    return;
+                }
+                break;
+            case DaizaCmd::Goal::EXPAND_AND_UNCLAMP:
+                if(step == 0){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = 1;
+                    act.clamp  = state.is_clamp;
+                    act.guide_close = state.is_guide_close;
+                    mech.set_daiza(act);
+
+                    if(state.is_expand == 1){
+                        feedback->feedback = DaizaCmd::Feedback::EXPANDED_AT_EXPAND_AND_UNCLAMP;
+                        goal_handle->publish_feedback(feedback);
+                        RCLCPP_INFO(this->get_logger(), "EXPANDED_AT_EXPAND_AND_UNCLAMP");
+                        step++;
+                    }
+                }else if(step == 1){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = 1;
+                    act.clamp  = 0;
+                    act.guide_close = 0;
+                    mech.set_daiza(act);
+
+                    if(state.is_clamp == 0 && state.is_guide_close == 0){
+                        result->result = DaizaCmd::Result::OK;
+                        goal_handle->succeed(result);
+                        RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::EXPAND_AND_UNCLAMP OK");
+                        return;
+                    }
+                }
+                break;
+            case DaizaCmd::Goal::CLAMP_AND_CONTRACT:
+                if(step == 0){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = state.is_expand;
+                    act.clamp  = state.is_clamp;
+                    act.guide_close = 1;
+                    mech.set_daiza(act);
+
+                    if(state.is_guide_close == 1){
+                        // feedback->feedback = DaizaCmd::Feedback::CLAMPED_AT_CLAMP_AND_CONTRACT;
+                        // goal_handle->publish_feedback(feedback);
+                        // RCLCPP_INFO(this->get_logger(), "CLAMPED_AT_CLAMP_AND_CONTRACT");
+                        step++;
+                    }
+                }else if(step == 1){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = state.is_expand;
+                    act.clamp  = 1;
+                    act.guide_close = 1;
+                    mech.set_daiza(act);
+
+                    if(state.is_clamp == 1){
+                        feedback->feedback = DaizaCmd::Feedback::CLAMPED_AT_CLAMP_AND_CONTRACT;
+                        goal_handle->publish_feedback(feedback);
+                        RCLCPP_INFO(this->get_logger(), "CLAMPED_AT_CLAMP_AND_CONTRACT");
+                        step++;
+                    }
+                }else if(step == 2){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = 0;
+                    act.clamp  = 1;
+                    act.guide_close = 1;
+                    mech.set_daiza(act);
+
+                    if(state.is_expand == 0){
+                        result->result = DaizaCmd::Result::OK;
+                        goal_handle->succeed(result);
+                        RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::CLAMP_AND_CONTRACT OK");
+                        return;
+                    }
+                }
+                break;
+            case DaizaCmd::Goal::EXPAND_AND_PLACE:
+                if(step == 0){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = 1;
+                    act.clamp  = state.is_clamp;
+                    act.guide_close = state.is_guide_close;
+                    mech.set_daiza(act);
+
+                    if(state.is_expand == 1){
+                        feedback->feedback = DaizaCmd::Feedback::EXPANDED_AT_EXPAND_AND_PLACE;
+                        goal_handle->publish_feedback(feedback);
+                        RCLCPP_INFO(this->get_logger(), "EXPANDED_AT_EXPAND_AND_PLACE");
+                        step++;
+                    }
+                }else if(step == 1){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = 1;
+                    act.clamp  = 0;
+                    act.guide_close = state.is_guide_close;
+                    mech.set_daiza(act);
+
+                    if(state.is_clamp == 0){
+                        result->result = DaizaCmd::Result::OK;
+                        goal_handle->succeed(result);
+                        RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::EXPAND_AND_PLACE OK");
+                        return;
+                    }
+                }
+                break;
+            case DaizaCmd::Goal::EXPAND_AND_PLACE_AND_CONTRACT:
+                if(step == 0){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = 1;
+                    act.clamp  = state.is_clamp;
+                    act.guide_close = state.is_guide_close;
+                    mech.set_daiza(act);
+
+                    if(state.is_expand == 1){
+                        feedback->feedback = DaizaCmd::Feedback::EXPANDED_AT_EXPAND_AND_PLACE_AND_CONTRACT;
+                        goal_handle->publish_feedback(feedback);
+                        RCLCPP_INFO(this->get_logger(), "EXPANDED_AT_EXPAND_AND_PLACE_AND_CONTRACT");
+                        step++;
+                    }
+                }else if(step == 1){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = 1;
+                    act.clamp  = 0;
+                    act.guide_close = state.is_guide_close;
+                    mech.set_daiza(act);
+
+                    if(state.is_clamp == 0){
+                        feedback->feedback = DaizaCmd::Feedback::PLACE_AT_EXPAND_AND_PLACE_AND_CONTRACT;
+                        goal_handle->publish_feedback(feedback);
+                        RCLCPP_INFO(this->get_logger(), "PLACE_AT_EXPAND_AND_PLACE_AND_CONTRACT");
+                        step++;
+                    }
+                }else if(step == 2){
+                    struct DaizaState state = get_daiza();
+                    struct DaizaActuator act;
+                    act.expand = 0;
+                    act.clamp  = 0;
+                    act.guide_close = state.is_guide_close;
+                    mech.set_daiza(act);
+
+                    if(state.is_expand == 0){
+                        result->result = DaizaCmd::Result::OK;
+                        goal_handle->succeed(result);
+                        RCLCPP_INFO(this->get_logger(), "DaizaCmd::Goal::EXPAND_AND_PLACE_AND_CONTRACT OK");
+                        return;
+                    }
+                }
+                break;
+            default:
+                result->result = DaizaCmd::Result::ERR_UNEXPECTED_ARG;
+                goal_handle->succeed(result);
+                RCLCPP_INFO(this->get_logger(), "ERR_UNEXPECTED_ARG");
+                return;
+            }
+            RCLCPP_INFO(this->get_logger(), "loop: %d", i);
+            loop.sleep();
+        }
+
+        
+        result->result = DaizaCmd::Result::ERR_TIMEOUT;
+        goal_handle->succeed(result);
+        RCLCPP_INFO(this->get_logger(), "ERR_TIMEOUT");
+    }
+
+    void set_bonbori_callback(const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
+        std::shared_ptr<std_srvs::srv::SetBool::Response> response) const
+    {
+        response->success = true;
+        response->message = "";
+        RCLCPP_INFO(this->get_logger(), "bonbori: %d", request->data);
+    }
+};
--- src/mech.hpp
+++ src/mech.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "rclcpp/rclcpp.hpp"
 #include "mecha_control/msg/sensor_states.hpp"
 #include <iostream>
@@ -5,64 +6,106 @@
 #include <mutex>
 #include <unistd.h>
 
-template<typename _Node>
+#include "cmd_seq_class.hpp"
+
+
 class Mech
 {
+public:
+    struct DaizaState{
+        bool is_expand = 0;
+        bool is_clamp = 0;
+        bool is_guide_close = 0;
+    };
+    struct HinaState{
+        float angle = 0;
+        bool is_pos_carry = 0;
+        bool is_pos_take = 0;
+        bool is_pos_place = 0;
+        bool is_up = 0;
+        bool is_down = 0;
+        bool launched_hina_1 = 0;
+        bool launched_hina_2 = 0;
+    };
+    struct DaizaActuator{
+        bool expand = 0;
+        bool clamp = 0;
+        bool guide_close = 0;
+    };
+    struct HinaActuator{
+        float angle = 0;
+        bool up = 0;
+        bool guide_expand  = 0;
+        bool launch_hina_1 = 0;
+        bool launch_hina_2 = 0;
+    };
+
+    Mech(rclcpp::Node *node){
+        this->daiza_pub_ = node->create_publisher<mecha_control::msg::ActuatorCommands>("daiza_clamp", 10);
+        this->hina_pub_ = node->create_publisher<mecha_control::msg::ActuatorCommands>("hina_dastpan", 10);
+        this->daiza_sensor_sub_ = node->create_subscription<mecha_control::msg::SensorStates>(
+            "daiza_state", 10, std::bind(&Mech::daiza_sensor_callback, node, std::placeholders::_1));
+        this->hina_sensor_sub_  = node->create_subscription<mecha_control::msg::SensorStates>(
+            "hina_state", 10, std::bind(&Mech::hina_sensor_callback, node, std::placeholders::_1));
+    }
+    struct DaizaState get_daiza(){
+        this->daiza_mutex.lock();
+        struct DaizaState ret = this->daiza;
+        this->daiza_mutex.unlock();
+        return ret;
+    }
+    struct HinaState get_hina(){
+        this->hina_mutex.lock();
+        struct HinaState ret = this->hina;
+        this->hina_mutex.unlock();
+        return ret;
+    }
+    void set_daiza(struct DaizaActuator act){
+        auto message = mecha_control::msg::ActuatorCommands();
+        message.cylinder_states.resize(4, false);
+        message.cylinder_states[0] = act.clamp;
+        message.cylinder_states[1] = act.clamp;
+        message.cylinder_states[2] = act.guide_close;
+        message.cylinder_states[3] = act.expand;
+        daiza_pub_->publish(message);
+    }
+    void set_hina(struct HinaActuator act){
+        auto message = mecha_control::msg::ActuatorCommands();
+        message.cylinder_states.resize(2, false);
+        message.motor_expand.resize(1, false);
+        message.motor_positions.resize(3, false);
+        message.cylinder_states[0] = act.launch_hina_1;
+        message.cylinder_states[1] = act.launch_hina_2;
+        message.motor_expand[0]    = act.up;
+        message.motor_positions[0] = act.angle;
+        message.motor_positions[1] = act.guide_expand ? 0.0 : 0.78539816339745;
+        message.motor_positions[2] = act.guide_expand ? 0.0 : -0.78539816339745;
+        daiza_pub_->publish(message);
+    }
 private:
     std::thread mech_observer;
-    rclcpp::Publisher<mecha_control::msg::ActuatorCommands>::SharedPtr daiza_pub;
-    rclcpp::Publisher<mecha_control::msg::ActuatorCommands>::SharedPtr hina_pub;
+    rclcpp::Publisher<mecha_control::msg::ActuatorCommands>::SharedPtr daiza_pub_;      // cylinder_states.size()==4 && motor_expand.size()==0 && motor_positions.size()==0
+    rclcpp::Publisher<mecha_control::msg::ActuatorCommands>::SharedPtr hina_pub_;       // cylinder_states.size()==2 && motor_expand.size()==1 && motor_positions.size()==3
     rclcpp::Subscription<mecha_control::msg::SensorStates>::SharedPtr daiza_sensor_sub_;
     rclcpp::Subscription<mecha_control::msg::SensorStates>::SharedPtr hina_sensor_sub_;
     std::mutex daiza_mutex;
-    DaizaState daiza;
+    struct DaizaState daiza;
     std::mutex hina_mutex;
-    HinaState  hina;
-    void daiza_sensor_callback(const mecha_control::msg::SensorStates message) const {
+    struct HinaState  hina;
+    void daiza_sensor_callback(const mecha_control::msg::SensorStates message) {
         this->daiza_mutex.lock();
         this->daiza.is_clamp        = message.cylinder_states[0];
         this->daiza.is_guide_close  = message.cylinder_states[2];
-        this->daiza.is_extract      = message.cylinder_states[3];
+        this->daiza.is_expand      = message.cylinder_states[3];
         this->daiza_mutex.unlock();
     }
-    void hina_sensor_callback(const mecha_control::msg::SensorStates message) const {
+    void hina_sensor_callback(const mecha_control::msg::SensorStates message) {
         this->hina_mutex.lock();
         this->hina.angle         = message.potentiometer_angles[0];
         this->hina.is_up         = message.limit_switch_states[0];
         this->hina.is_down       = message.limit_switch_states[1];
-        this->hina.hina_launch_1 = message.cylinder_states[0];
-        this->hina.hina_launch_2 = message.cylinder_states[1];
-        this->hina_mutex.unlock()
-    }
-public:
-    Mech(_Node *node){
-        this->daiza_sensor_sub_ = create_subscription<mecha_control::msg::SensorStates>(
-            "daiza_state", 10, std::bind(&MechSeqNode::daiza_sensor_callback, node, std::placeholders::_1));
-        this->hina_sensor_sub_  = create_subscription<mecha_control::msg::SensorStates>(
-            "hina_state", 10, std::bind(&MechSeqNode::hina_sensor_callback, node, std::placeholders::_1));
-    }
-    DaizaState get_daiza(){
-        this->daiza_mutex.lock();
-        DaizaState ret = this->daiza;
-        this->daiza_mutex.unlock();
-        return ret;
-    }
-    HinaState get_hina(){
-        this->hina_mutex.lock();
-        DaizaState ret = this->hina;
+        this->hina.launched_hina_1 = message.cylinder_states[0];
+        this->hina.launched_hina_2 = message.cylinder_states[1];
         this->hina_mutex.unlock();
-        return ret;
-    }
-    struct DaizaState{
-        bool is_extract;
-        bool is_clamp;
-        bool is_guide_close;
-    }
-    struct HinaState{
-        float angle;
-        bool is_up;
-        bool is_down;
-        bool hina_launch_1;
-        bool hina_launch_2;
     }
 };
